f313df5daf76aab1537ccb2069a3a6e00492af91
Zhang <zhang schuman@dva-c02tw0y1h.bcg.com>
2018-10-29 13:43:44 +1100

    more conflicts

--- src/components/desk.jsx
+++ src/components/desk.jsx
@@ -1,17 +1,9 @@
 import React from 'react'
 import classNames from 'classnames'
 
-<<<<<<< HEAD
 export default function Desk({x, y, occupied}){
     const deskClasses= classNames("desk", {"desk--occupied": occupied})
     return(
         <div className={deskClasses} style={{left: x, top: y}} onClick={e=>{console.log("desk")}}/>
-=======
-export default function Desk(props){
-    const {x, y, data} = props
-    const deskClasses= classNames("desk", {"desk--taken": !data.available})
-    return(
-        <rect className={deskClasses} style={{x: x, y: y}} onClick={e=>{console.log("desk")}}/>
->>>>>>> first commit
     )
 }
--- src/components/partition.jsx
+++ src/components/partition.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import PartitionRow from './partition-row'
-<<<<<<< HEAD
 import {desksConstants} from '../constants/desks-constants'
 
 export default function Partition(props){
@@ -12,19 +11,5 @@ export default function Partition(props){
     }
     return(
         arrPartitionRows
-=======
-
-export default function Partition(props){
-    const {x, y, columns, rows, data} = props
-    const deskSize = 50
-    let partitionRows = []
-    for(let i=0; i < rows; i++){
-        partitionRows.push(<PartitionRow key={i} x={x} y={y + i * deskSize} columns={columns} data={data.rows[i]}/>)
-    }
-    return(
-        <svg>
-            {partitionRows}
-        </svg>
->>>>>>> first commit
     )
 }
